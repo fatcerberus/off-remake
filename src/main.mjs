e5a9641dd17fbb6e5afe869101b1d1011752cc46
Bruce Pascoe <fatcerberus@icloud.com>
2018-08-31 01:20:12 -0400

    Finish opening narration
    Added some embellishments compared to the original script, as well as a
    nice bit of misdirection.  Unreliable narrator, indeed.

--- src/main.mjs
+++ src/main.mjs
@@ -4,7 +4,7 @@
  *  main.mjs
  */
 
-import { Console, Music, Scene, Thread } from 'sphere-runtime';
+import { Console, Music, Prim, Scene, Thread } from 'sphere-runtime';
 import MenuStrip from 'menu-strip';
 import SpriteImage from 'sprite-image';
 
@@ -31,23 +31,32 @@ class OFFGame
 
 	async start()
 	{
-		/*Music.play('@/music/fourteenResidents.ogg');
-		await this.title.run();*/
+		Music.play('@/music/fourteenResidents.ogg');
+		await this.title.run();
 		let image = new SpriteImage('@/batter.rss');
 		image.pose = 'south';
 		image.x = Surface.Screen.width / 2;
 		image.y = Surface.Screen.height / 2;
+		image.alpha = 0.0;
 		await new Scene()
-			//.adjustBGM(0.0, 300)
-			.talk("", false, 1.0, Infinity, "Welcome, Eaty Pig.")
+			.adjustBGM(0.0, 180)
+			.talk("", false, 1.0, Infinity,
+				"Welcome, Bruce. We have been anticipating your arrival for quite some time.",
+				"But we hardly have time for formalities, do we? Let's cut to the case.")
 			.call(() => image.start())
+			.tween(image, 60, 'linear', { alpha: 1.0 })
 			.talk("", false, 1.0, Infinity, "You have been assigned to a being known as The Batter.")
 			.talk("", false, 1.0, Infinity, "The Batter has an important mission. Be sure that it's accomplished.")
-			.pause(1.0)
+			.pause(60)
 			.talk("", false, 1.0, Infinity,
-				"The switch is now ON. We will let you both out in Zone 0, where you should elicit the aid of the one known as The Judge.",
-				"Good luck.")
+				"The switch is now ON, but it won't be for long. We will let you both out in Zone 0, where you should solicit the advice of the one known as The Judge.")
+			.tween(image, 60, 'linear', { alpha: 0.0 })
+			.talk("", false, 1.0, Infinity, "Good luck.", "You're going to need it.")
+			.pause(60)
 			.run();
+		Music.play(null);
+		Music.adjustVolume(1.0, 0);
+		image.stop();
 	}
 
 	log(...args)
