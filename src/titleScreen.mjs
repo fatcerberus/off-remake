e4b91152af3d357868c76ce293f775a0a2608a1a
Bruce Pascoe <fatcerberus@icloud.com>
2018-09-04 23:59:58 -0400

    Simplify title screen code
    The Thread class turns out to be overkill for most of the things I've
    been using it for.  I was able to remove a lot of boilerplate from the
    title screen module by avoiding it.

--- src/titleScreen.mjs
+++ src/titleScreen.mjs
@@ -8,26 +8,17 @@ import { Music, Prim, Scene, Thread } from 'sphere-runtime';
 import { MenuStrip } from '$/menuSystem';
 
 export default
-class TitleScreen extends Thread
+class TitleScreen
 {
 	constructor(fileName = '@/data/titleScreen.json')
 	{
-		super();
-
 		this.data = require(fileName);
-		this.fadeAlpha = 0.0;
-		this.fadeTime = this.data.titleFadeFrames;
 		this.menu = new MenuStrip(this.data.menuText, false, [ "new game", "exit" ]);
 		this.showSplash = true;
 		this.texture = new Texture(this.data.titleScreen);
-		this.splashes = [];
-		for (const splash of this.data.splashScreens) {
-			let texture = new Texture(splash.fileName);
-			let thread = new SplashThread(texture, this.data.splashFadeFrames, splash.holdFrames);
-			this.splashes.push({ thread });
-		}
-
-		this.start();
+		this.splashScene = new Scene();
+		for (const splash of this.data.splashScreens)
+			this.splashScene.splash(splash.fileName, this.data.splashFadeFrames, splash.holdFrames);
 	}
 
 	async run(forceSplash = false)
@@ -35,78 +26,32 @@ class TitleScreen extends Thread
 		if (this.data.musicOverSplash)
 			Music.play(this.data.music);
 		if (this.showSplash || forceSplash) {
-			for (const splash of this.splashes) {
-				splash.thread.start();
-				await Thread.join(splash.thread);
-			}
+			await this.splashScene.run();
 			if (!this.data.alwaysShowSplash)
 				this.showSplash = false;
 		}
 		if (!this.data.musicOverSplash)
 			Music.play(this.data.music);
+		let fadeMask = Color.White.fadeTo(0.0);
+		let job = Dispatch.onRender(() => {
+			Prim.blit(Surface.Screen, 0, 0, this.texture, fadeMask);
+		});
 		await new Scene()
-			.tween(this, this.fadeTime, 'linear', { fadeAlpha: 1.0 })
+			.tween(fadeMask, this.data.titleFadeFrames, 'linear', { a: 1.0 })
 			.run();
 		await this.menu.run();
 		await new Scene()
 			.doIf(() => !this.data.persistBGM)
 				.fork()
-					.adjustBGM(0.0, this.fadeTime)
+					.adjustBGM(0.0, this.data.titleFadeFrames)
 					.changeBGM(null)
 				.end()
 			.end()
-			.tween(this, this.fadeTime, 'linear', { fadeAlpha: 0.0 })
+			.tween(fadeMask, this.data.titleFadeFrames, 'linear', { a: 0.0 })
 			.resync()
 			.adjustBGM(1.0)
 			.run();
-	}
-
-	on_startUp()
-	{
-		this.fadeAlpha = 0.0;
-	}
-
-	on_render()
-	{
-		let fadeMask = Color.White.fadeTo(this.fadeAlpha);
-		Prim.blit(Surface.Screen, 0, 0, this.texture, fadeMask);
-	}
-}
-
-class SplashThread extends Thread
-{
-	constructor(texture, fadeTime, holdTime)
-	{
-		super();
-
-		this.fadeStep = 1.0 / fadeTime;
-		this.holdTime = holdTime;
-		this.texture = texture;
-		this.x = Math.trunc((Surface.Screen.width - texture.width) / 2);
-		this.y = Math.trunc((Surface.Screen.height - texture.height) / 2);
-	}
-
-	on_startUp()
-	{
-		this.fadeAlpha = 0.0;
-		this.fadeMask = Color.White.fadeTo(0.0);
-	}
-
-	on_render()
-	{
-		Prim.blit(Surface.Screen, this.x, this.y, this.texture, this.fadeMask);
-	}
-
-	async on_update()
-	{
-		this.fadeAlpha = Math.min(Math.max(this.fadeAlpha + this.fadeStep, 0.0), 1.0);
-		this.fadeMask = Color.White.fadeTo(this.fadeAlpha);
-		if (this.fadeAlpha >= 1.0) {
-			await Sphere.sleep(this.holdTime);
-			this.fadeStep = -(this.fadeStep);
-		}
-		else if (this.fadeAlpha <= 0.0) {
-			this.stop();
-		}
+		job.cancel();
+		
 	}
 }
